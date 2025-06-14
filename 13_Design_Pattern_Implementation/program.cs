700d94cc5013f7192df21e2134d00af3fac4ff37
Dhafin.Kamil <53680609+PliTheGeek@users.noreply.github.com>
2025-06-14 23:55:15 +0700

    Add Project Jounal Singleton

---
+++ 13_Design_Pattern_Implementation/program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var pusatData = PusatDataSingleton.GetDataSingleton();
+
+        pusatData.AddSebuahData("Belajar Singleton");
+        pusatData.AddSebuahData("Design Pattern");
+        pusatData.AddSebuahData("C# .NET 8");
+
+        Console.WriteLine("Semua Data:");
+        pusatData.PrintSemuaData();
+
+        pusatData.HapusSebuahData(1);
+
+        Console.WriteLine("\nSetelah menghapus data index 1:");
+        pusatData.PrintSemuaData();
+    }
+}
